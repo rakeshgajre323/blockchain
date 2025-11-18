f00e74235706a573db4156e1855bb25fa6fec346
Rakesh Gajre <rakeshgajre.work@gmail.com>
2025-11-18 23:03:32 +0530

    backend working on port 5000

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// backend/server.js
 import express from "express";
 import cors from "cors";
 import compression from "compression";
@@ -8,18 +7,15 @@ dotenv.config();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(compression());
 
-// Simple test route
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok", message: "Backend is working 🚀" });
 });
 
-// Use PORT from Render or default to 10000
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
